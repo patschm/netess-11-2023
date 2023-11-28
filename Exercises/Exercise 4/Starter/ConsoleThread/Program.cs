c13282c1abbff4942f59a281cc1b2d7a5c58f4a3
Patrick Schmidt <pschmidt@pasit.nl>
2023-11-28 10:44:45 +0100

    day2

---
+++ Exercises/Exercise 4/Starter/ConsoleThread/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Threading.Channels;
+
+namespace ConsoleThread;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //CalculateSync();
+        CalculateASync();
+        Console.WriteLine("Terug in Main");
+        Console.ReadLine();
+    }
+
+    private static void CalculateSync()
+    {
+        int result = LongAdd(2, 3);
+        Console.WriteLine($"Het resultaat is {result}");
+        //Console.WriteLine("Het resultaat is {0}", result);
+    }
+    private static void CalculateASync()
+    {
+        //Func<int, int, int> del1 = LongAdd;
+
+        //IAsyncResult ar = del1.BeginInvoke(3, 4, ax => {
+        //    int result = del1.EndInvoke(ax);
+
+        //    Console.WriteLine($"Het resultaat is {result}");
+        //}, null);
+
+        //Task t1 = new Task(() => {
+        //    int result = LongAdd(2, 3);
+        //    Console.WriteLine($"Het resultaat is {result}");
+        //});
+        //t1.Start();
+        // Task.WaitAll(t1);
+
+        var t2 = Task.Run(() =>
+        {
+            int result = LongAdd(2, 3);
+            return result;          
+        });
+        t2.ContinueWith(pt => Console.WriteLine($"Het resultaat is {pt.Result}"));
+
+        //Console.WriteLine($"Het resultaat is {t2.Result}");
+        //t2.ContinueWith(pt => Console.WriteLine(pt.Status))
+        //    .ContinueWith(pt=>Console.WriteLine("Doen we ook"));
+
+
+
+    }
+
+    static int LongAdd(int a, int b)
+    {
+        Task.Delay(5000).Wait();
+        return a + b;
+    }
+}
---
+++ Exercises/Exercise 4/Starter/Inlines/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Inlines;
+
+delegate int MatDel(int x, int y);
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int c = 100;
+        MatDel m1 = Add;
+        MatDel m2 = (a, b) => a + b + c;     
+
+        Console.WriteLine(m2(4,8));
+
+    }
+
+    static int Add(int a, int b)
+    {
+            return a + b;
+    }
+}
